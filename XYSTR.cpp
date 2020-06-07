6ecb082e2cf5e7fac1fd4bee9bed62debeb1fb9e
GiriTejaBille <giriteja94495@gmail.com>
2020-06-07 21:45:19 +0530

    Create XYSTR.cpp

---
+++ XYSTR.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h> using namespace std; int main(){ int t; cin>>t; while(t--){ string s; cin>>s; int i=0,count=0; while(i<s.size()-1){ if(s[i]!=s[i+1]){ count++; i+=2; } else{ i++; } } cout<<count<<endl; } }
